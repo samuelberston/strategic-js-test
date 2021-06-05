3b1208d507b3d5759213cc24504c61672f51714c
Sam Berston <samuelberston@gmail.com>
2021-06-04 18:07:52 -0700

    select function and calculateTotal function

--- client/src/components/Account.jsx
+++ client/src/components/Account.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
-const Account = ({ account }) => {
+const Account = ({ account, select }) => {
   const {
     id,
     creditorName,
@@ -11,19 +11,32 @@ const Account = ({ account }) => {
     balance,
   } = account;
   return (
-    <div>
-      {id}
-      {creditorName}
-      {firstName}
-      {lastName}
-      {minPaymentPercentage}
-      {balance}
+    <div id={id}>
+      <div id="select" onClick={select} value={id}>
+        <input type="checkbox" value={id} />
+      </div>
+      <div id="creditor">
+        {creditorName}
+      </div>
+      <div id="firstName">
+        {firstName}
+      </div>
+      <div id="lastName">
+        {lastName}
+      </div>
+      <div id="minPay%">
+        {minPaymentPercentage}
+      </div>
+      <div id="balance">
+        {balance}
+      </div>
     </div>
   );
 };
 
 Account.propTypes = {
   account: PropTypes.objectOf(PropTypes.any).isRequired,
+  select: PropTypes.func.isRequired,
 };
 
 export default Account;
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,40 +7,97 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      accountData: null,
+      accounts: null,
+      selected: [],
+      total: null,
     };
-    this.getAccountData = this.getAccountData.bind(this);
+    this.getAccounts = this.getAccounts.bind(this);
+    this.select = this.select.bind(this);
+    this.calculateTotal = this.calculateTotal.bind(this);
   }
 
   componentDidMount() {
-    this.getAccountData();
+    this.getAccounts();
   }
 
-  getAccountData() {
+  getAccounts() {
     axios.get('/data')
       .then((res) => {
         this.setState({
-          accountData: res.data,
+          accounts: res.data,
         });
       });
   }
 
+  select(e) {
+    const { accounts, selected } = this.state;
+    const id = selected.indexOf(Number(e.target.value));
+    if (id !== -1) {
+      selected.splice(id, 1);
+      this.setState({
+        selected,
+      });
+    } else {
+      accounts.forEach((account) => {
+        console.log(typeof e.target.value, typeof account.id);
+        if (Number(e.target.value) === Number(account.id)) {
+          selected.push(account.id);
+          this.setState({
+            selected,
+          });
+        }
+      });
+    }
+    this.calculateTotal();
+  }
+
+  calculateTotal() {
+    let total = 0;
+    const { selected, accounts } = this.state;
+
+    accounts.forEach((account) => {
+      if (selected.includes(account.id)) {
+        total += account.balance;
+      }
+    });
+
+    this.setState({
+      total,
+    });
+  }
+
   render() {
-    let { accountData } = this.state;
+    const { accounts } = this.state;
     return (
       <div>
         <h1>
           Account Data
         </h1>
-        {accountData
-          ? (
-            <div id="DataTable">
-              {accountData.map((account) => (
-                <Account account={account} />
-              ))}
+        <div id="dataTable">
+          <div id="columns">
+            <div id="creditor">
+              Creditor
             </div>
-          )
-          : ''}
+            <div id="firstName">
+              First Name
+            </div>
+            <div id="lastName">
+              Last Name
+            </div>
+            <div id="minPay%">
+              Min Pay %
+            </div>
+            <div id="balance">
+              Balance
+            </div>
+          </div>
+          {accounts
+            ? (
+              accounts.map((account) => (
+                <Account account={account} select={this.select} />
+              )))
+            : ''}
+        </div>
       </div>
     );
   }
